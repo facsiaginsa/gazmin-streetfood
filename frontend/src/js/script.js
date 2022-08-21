d9e547b45a2d6e3b98575e862ad4debea8c54077
Faizal Chan <moefc32@gmail.com>
2022-08-22 05:52:33 +0700

    ganti isi frontend

---
+++ frontend/src/js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function callModal(content) {
+    switch (content) {
+        case 'login':
+            $('#modal-container').addClass('auth');
+            $('#modal-container').removeClass('hidden');
+            $('#modal-container').load('login.html');
+            break;
+        case 'register':
+            $('#modal-container').addClass('auth');
+            $('#modal-container').removeClass('hidden');
+            $('#modal-container').load('register.html');
+            break;
+        case 'search':
+            $('#modal-container').removeClass('auth');
+            $('#modal-container').removeClass('hidden');
+            $('#modal-container').load('search.html');
+            break;
+        case 'menu':
+            $('#modal-container').removeClass('auth');
+            $('#modal-container').removeClass('hidden');
+            $('#modal-container').load('menu.html');
+            break;
+        case 'checkout':
+            $('#modal-container').removeClass('auth');
+            $('#modal-container').removeClass('hidden');
+            $('#modal-container').load('checkout.html');
+            break;
+        default:
+            $('#modal-container').removeClass('auth');
+            $('#modal-container').addClass('hidden');
+            $('#modal-container').val('');
+    }
+};
