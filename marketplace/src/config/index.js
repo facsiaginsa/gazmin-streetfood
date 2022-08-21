8d217e50b53795e53fa7541323c741545e25d8ae
Bagus Facsi Aginsa <facsi.aginsa@gmail.com>
2022-08-21 07:24:40 +0700

    change default port

--- marketplace/src/config/index.js
+++ marketplace/src/config/index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 module.exports = {
     host: process.env.IPBIND || "0.0.0.0",
-    port: parseInt(process.env.PORT) || 4000,
+    port: parseInt(process.env.PORT) || 3000,
     db: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
