64a961cf1d66a592cccbdcf68b1234ca56060460
Bagus Facsi Aginsa <910155@telkom.co.id>
2022-08-27 08:35:47 +0700

    fix bug

--- marketplace/src/services/getExistingCart.js
+++ marketplace/src/services/getExistingCart.js
@@ -1,10 +1,14 @@
 const { jsonParse } = require("../loaders/fast-json")
 const { getCart } = require("../models/cart")
+const { searchAllProduct } = require("../models/product")
+const { selectStallById } = require("../models/stall")
+const formatConvert = require("./formatConvert")
+const productIdToObject = require("./productIdToObject")
 
 module.exports = async (user_id) => {
     // let code, message
-    
-    let cart =  jsonParse(await getCart(user_id)).value
+    let [stringCart, products] = await Promise.all([getCart(user_id), searchAllProduct()])
+    let cart = jsonParse(stringCart).value
 
     if (!cart) {
         return {
@@ -12,6 +16,15 @@ module.exports = async (user_id) => {
             message: "cart is empty"
         }
     }
+
+    let stall = await selectStallById(cart.stall)
+    stall.id = cart.stall
+    cart.stall = stall
+
+    // modified products value
+    let formattedProduct = await formatConvert(products)
+    let modifiedProduct = cart.products.map(productIdToObject, formattedProduct)
+    cart.products = modifiedProduct
 
     return {
         code: 0,
